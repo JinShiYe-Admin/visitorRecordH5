46fe694a89d2ad24310577a038b5f5bcd2758f57
LuckyCX <721685743@qq.com>
2021-05-15 10:33:17 +0800

    新增读卡js访问原生API

---
+++ js/utils/idCardReader.js
@@ -0,0 +1,23 @@
+document.addEventListener("plusready", function() {
+	var _BARCODE = 'idCardReaderUtil',
+		B = window.plus.bridge;
+	
+	var idCardReader = {
+		//读取身份证信息
+		readIDCard: function(successCallback, errorCallback) {
+			var success = typeof successCallback !== 'function' ? null : function(args) {
+					successCallback(args);
+				},
+				fail = typeof errorCallback !== 'function' ? null : function(code) {
+					errorCallback(code);
+				};
+			callbackID = B.callbackId(success, fail);
+			return B.exec(_BARCODE,'readIDCard', [callbackID]);
+		},
+		//播放读取音
+		playBeep: function() {
+			B.exec(_BARCODE,'playBeep', []);
+		},
+	};
+	window.plus.idCard = idCardReader;
+}, true);
